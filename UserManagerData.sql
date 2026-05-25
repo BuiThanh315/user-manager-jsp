9222d2daa10265d873a27e969b401b210b754f7d
BuiThanh9935 <robluccibn040@gmail.com>
2026-05-25 14:04:40 +0700

    thirth database

--- UserManagerData.sql
+++ UserManagerData.sql
@@ -23,3 +23,19 @@ INSERT INTO users (name, email, country) VALUES
 ('Bob Marley', 'bob.marley@example.com', 'Jamaica'),
 ('David Beckham', 'david.b@example.com', 'United Kingdom'),
 ('Nguyen Minh Tam', 'tam.nguyen@example.com', 'Viet Nam');
+
+DELIMITER $$
+create procedure get_user_by_id(in user_id int)
+begin
+ select users.name, users.email, users.country
+ from users
+ where users.id = user_id;
+ end$$
+DELIMITER ;
+
+DELIMITER $$
+create procedure insert_user(in user_name varchar(50),in user_email varchar(50),in user_country varchar(50))
+begin
+ insert into users(name, email, country) values(user_name, user_email, user_country);
+ end$$
+DELIMITER ;
